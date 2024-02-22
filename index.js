da6e9c78c03686a0918c715a7ffd3469f7502f8a
arnald18 <acenaldy04@gmail.com>
2024-02-21 19:08:45 -0500

    add license question, handle answers

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const questions = [
     name: "title",
     message: "What's the title of the project?",
   },
+
+  {
+    name: "description",
+    type: "input",
+    message: "What's the description of your project?",
+  },
+  {
+    name: "license",
+    type: "list",
+    message: "What license are you using?",
+    choices: ["MIT", "ISC", "Apache 2.0"],
+  },
 ];
 
-inquirer.prompt(questions);
+inquirer.prompt(questions).then((answers) => {
+  console.log(answers);
+});
